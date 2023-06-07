3f137d29741833da251c56e887ff8514c0f3342b
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-06-07 18:05:47 +0600

    header,footer and banner created

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,31 @@
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+import banner1 from '../../assets/images/slider1.png'
+import banner2 from '../../assets/images/slider2.png'
+import banner3 from '../../assets/images/slider3.png'
+
+const Banner = () => {
+    return (
+
+
+        <Carousel>
+            <div>
+                <img src={banner1} />
+
+            </div>
+            <div>
+                <img src={banner2} />
+
+            </div>
+            <div>
+                <img src={banner3} />
+
+            </div>
+        </Carousel>
+
+    );
+};
+
+export default Banner;
