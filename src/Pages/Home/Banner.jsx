5a30d83e9f91cf86692eb273e77069cea6f39762
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-06-11 23:14:08 +0600

    Banner fixed

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,9 +1,9 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
-import banner1 from '../../assets/images/image1.jpg'
-import banner2 from '../../assets/images/slider2.png'
-import banner3 from '../../assets/images/slider3.png'
+import banner1 from '../../assets/images/image1.jpg';
+import banner2 from '../../assets/images/slider2.png';
+import banner3 from '../../assets/images/slider3.png';
 
 const Banner = () => {
   return (
@@ -23,10 +23,10 @@ const Banner = () => {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-3xl font-bold text-white">Unlock the World of Languages</h1>
-            <h1 className="mb-5 text-xl font-semibold text-white">Expand your horizons, embrace diversity</h1>
-            <p className="mb-5 text-white">Immerse yourself in the beauty of languages. Discover new cultures, make meaningful connections, and gain valuable skills. Join our Foreign Language Learning School and embark on a linguistic adventure!</p>
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="mb-5 text-3xl font-bold text-white">Learn Languages with Ease</h1>
+            <h1 className="mb-5 text-xl font-semibold text-white">Master communication in multiple languages</h1>
+            <p className="mb-5 text-white">Experience effortless language learning with our proven methods and interactive approach. Gain fluency and confidence in expressing yourself in diverse linguistic contexts.</p>
+            <button className="btn btn-primary">Join Now</button>
           </div>
         </div>
       </div>
@@ -34,10 +34,10 @@ const Banner = () => {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-3xl font-bold text-white">Unlock the World of Languages</h1>
-            <h1 className="mb-5 text-xl font-semibold text-white">Expand your horizons, embrace diversity</h1>
-            <p className="mb-5 text-white">Immerse yourself in the beauty of languages. Discover new cultures, make meaningful connections, and gain valuable skills. Join our Foreign Language Learning School and embark on a linguistic adventure!</p>
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="mb-5 text-3xl font-bold text-white">Explore Global Cultures</h1>
+            <h1 className="mb-5 text-xl font-semibold text-white">Discover the richness of cultural diversity</h1>
+            <p className="mb-5 text-white">Immerse yourself in the traditions, customs, and histories of various cultures. Expand your worldview, develop intercultural competence, and forge connections that transcend borders.</p>
+            <button className="btn btn-primary">Discover More</button>
           </div>
         </div>
       </div>
