05016a2f1dfc4ea13d6dcc9922b5673795eb3507
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-06-14 23:07:35 +0600

    firebase and .env file added

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,48 +1,115 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { AuthContext } from 'path/to/AuthProvider';
+import { getAuth, signOut } from 'firebase/auth';
+import app from 'path/to/firebase/config';
+import logo from 'path/to/logo.png';
 
-const NavBar = () => {
+const Navbar = () => {
+    const { user } = useContext(AuthContext);
+    const [isHovered, setIsHovered] = useState(false);
 
+    const handleHover = () => {
+        setIsHovered(!isHovered);
+    };
 
-    const navOptions = <>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/allclasses">Classes</Link></li>
-        <li><Link to="/allclasses">Classes</Link></li>
-        <li><Link to="/allclasses">Classes</Link></li>
-        <li><Link to="/allclasses">Classes</Link></li>
- 
-    </>
+    const navOptions = (
+        <>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/allclasses">Classes</Link></li>
+            <li><Link to="/allclasses">Classes</Link></li>
+            <li><Link to="/allclasses">Classes</Link></li>
+            <li><Link to="/allclasses">Classes</Link></li>
+        </>
+    );
 
+    const auth = getAuth(app);
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then()
+            .catch(error => {
+                console.log(error);
+            });
+    };
 
     return (
-        <div className="navbar fixed z-10 bg-base-100 font-bold max-w-screen-xl">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {navOptions}
-                    </ul>
-                </div>
-                
-                <div>
-                    <Link >
-                       <small className="text-xl"> PolyLingual</small> <br />
-                       <small> Summer Quest</small>
+        <div>
+            <div className="navbar bg-base-100 lg:h-24">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            {navOptions}
+                        </ul>
+                    </div>
 
+                    <Link to="/" className="lg:text-3xl font-bold lg:m-1 lg:p-1">
+                        <div className="flex items-center">
+                            <div>
+                                <img src={logo} alt="" />
+                            </div>
+                            <div>IntellectoToys</div>
+                        </div>
                     </Link>
                 </div>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navOptions}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+                </div>
+
+                <div className="navbar-end">
+                    {user ? (
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center">
+                                {user.photoURL && (
+                                    <label
+                                        tabIndex={0}
+                                        className="btn btn-ghost btn-circle avatar"
+                                        onMouseEnter={handleHover}
+                                        onMouseLeave={handleHover}
+                                    >
+                                        <div className="w-10 rounded-full">
+                                            <img src={user.photoURL} alt="" />
+                                        </div>
+                                        {isHovered && (
+                                            <span className="username">
+                                                {user.displayName ? user.displayName : <></>}
+                                            </span>
+                                        )}
+                                    </label>
+                                )}
+                                <button onClick={handleSignOut} className="btn">
+                                    Logout
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <Link to="/login">
+                            <span className="btn">Login</span>
+                        </Link>
+                    )}
+                </div>
             </div>
         </div>
     );
 };
 
-export default NavBar;
+export default Navbar;
